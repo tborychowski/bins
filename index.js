696a546573ce30c845f20bea31f940a8a8cde373
Tom <tborychowski@gmail.com>
2016-01-02 17:36:51 +0000

    tweak exports

--- index.js
+++ index.js
@@ -3,20 +3,23 @@
 const list = require('./src/list');
 const empty = require('./src/empty');
 
-module.exports = { list, empty };
+module.exports = {
+	get: () => list.get().then(list.total),
+	empty
+};
 
 
 
-list.get()
-	.then(bins => {
-		// console.log(1, bins);
-		return bins;
-	})
-	.then(list.total)
-	.then(bins => {
-		console.log(2, bins);
-	})
-	.catch(console.error.bind(console));
+// list.get()
+// 	.then(bins => {
+// 		// console.log(1, bins);
+// 		return bins;
+// 	})
+// 	.then(list.total)
+// 	.then(bins => {
+// 		console.log(2, bins);
+// 	})
+// 	.catch(console.error.bind(console));
 
 
 
